83364a336a7485435f86f212d98962bf38c9cd2b
aayush-ashok <54711854+aayush-ashok@users.noreply.github.com>
2019-10-17 17:40:53 +0530

    updated def

--- Codechef/NOV17/CSUBQ.cpp
+++ Codechef/NOV17/CSUBQ.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #define mp make_pair
 #define F first
 #define S second
+#define T third
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
 #define sortall(x) sort(all(x))
