8be81ef3ecd458bae1bd97a8809ab5b5b154f8a0
Rachit Jain <rachit.jain2095@gmail.com>
2020-05-25 12:18:45 +0530

    Z-Algorithm - bugfix - found in CC CookOff May20

--- Library/Miscellanious/z-algo.cpp
+++ Library/Miscellanious/z-algo.cpp
@@ -1,7 +1,10 @@
 //Z-Algorithm
 //z[i] is length of prefix starting from i
-int z[N];
-void go(string s){
+
+// always pass stirng by reference, weird how it got me SIGSEGV in a Codechef problem
+// https://codeforces.com/blog/entry/77906 
+vector<int> go(const string &s){
+	vector<int> z(s.size(), 0);
 	int i, n = s.size(), L = 0, R = 0;
 	z[0] = 0;
 	Fo(i, 1, n){
@@ -22,5 +25,31 @@ void go(string s){
 			}
 		}
 	}
-	
+
+	return z;
 }
+
+
+// another implementation
+template <typename T>
+vector<int> z_function(int n, const T &s) {
+  vector<int> z(n, n);
+  int l = 0, r = 0;
+  for (int i = 1; i < n; i++) {
+    z[i] = (i > r ? 0 : min(r - i + 1, z[i - l]));
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      z[i]++;
+    }
+    if (i + z[i] - 1 > r) {
+      l = i;
+      r = i + z[i] - 1;
+    }
+  }
+  return z;
+}
+
+template <typename T>
+vector<int> z_function(const T &s) {
+  return z_function((int) s.size(), s);
+}
+
